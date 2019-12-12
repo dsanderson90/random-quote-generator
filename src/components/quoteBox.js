fe51708c9fc356727096f9d40c17b3d685678209
dsanderson90 <dsanderson3@cnm.edu>
2019-12-12 09:41:29 -0700

    added tweet button

--- src/components/quoteBox.js
+++ src/components/quoteBox.js
@@ -22,6 +22,10 @@ export default class QuoteBox extends Component {
    })
  }
 
+ shareQuote = () => {
+  encodeURIComponent()
+ }
+
 
   render() {
     const Wrapper = styled.section`
@@ -54,6 +58,10 @@ const Button = styled.a`
   background: transparent;
   color: black;
   border: 2px solid pink;
+  text-decoration: none;
+  :hover {
+    transform: scale(1.2)
+  }
 
   /* The GitHub button is a primary button
    * edit this to target it specifically! */
@@ -68,6 +76,7 @@ const Button = styled.a`
         <Text id="text">"{this.state.currentQuote.text}"</Text>
         <Author id="author">- {this.state.currentQuote.author}</Author>
         <Button id="new-quote" onClick={this.getRandomQuote}>Get Random Quote</Button>
+        <Button target='_blank' href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(this.state.currentQuote.text)}&via=${encodeURIComponent(this.state.currentQuote.author)}`}id="tweet">Tweet</Button>
       </Wrapper>
     );
   }
